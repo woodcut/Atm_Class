3b5482b1a0c0903f1581406f519676fb8eed7f8a
Hank Tom <hanktom@gmail.com>
2016-09-26 20:45:57 +0800

    Snackbar元件

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -3,12 +3,16 @@ package com.tom.atm;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
     private EditText edUserid;
     private EditText edPasswd;
 
@@ -20,6 +24,28 @@ public class LoginActivity extends AppCompatActivity {
         String userid = getSharedPreferences("atm", MODE_PRIVATE)
                 .getString("PREF_USERID", "");
         edUserid.setText(userid);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_help:
+                Log.d(TAG, "action_help");
+                break;
+            case R.id.action_blabla:
+                Log.d(TAG, "action_blabla");
+                break;
+            case R.id.action_settings:
+                Log.d(TAG, "action_settings");
+                break;
+        }
+        return false;
     }
 
     private void findViews() {
