f6cac6c2306977a6ecc9d1edc65d4932f3c0f55e
Hank Tom <hanktom@gmail.com>
2016-09-29 19:35:18 +0800

    ListView事件處理

--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -3,23 +3,39 @@ package com.tom.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_LOGIN = 50;
+    private static final String TAG = "MainActivity";
     boolean logon = false;
-    String[] func = {"AAA", "BBB", "CCC", "DDD", "EEE"};
+//    String[] func = {"AAA", "BBB", "CCC", "DDD", "EEE"};
+    String[] func;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        func = getResources().getStringArray(R.array.functions);
         ListView list = (ListView) findViewById(R.id.list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, func);
         list.setAdapter(adapter);
-
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, position+"/"+id);
+                switch ((int)id){
+                    case 4:
+                        finish();
+                        break;
+                }
+            }
+        });
         if (!logon){
             Intent intent = new Intent(this, LoginActivity.class);
             startActivityForResult(intent, REQUEST_LOGIN);
