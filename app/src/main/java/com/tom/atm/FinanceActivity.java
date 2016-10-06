4d4a8d71280f6f14e74e8d11b6fc490fdfaa1609
Hank Tom <hanktom@gmail.com>
2016-10-06 21:01:26 +0800

    消費查詢

--- app/src/main/java/com/tom/atm/FinanceActivity.java
+++ app/src/main/java/com/tom/atm/FinanceActivity.java
@@ -1,14 +1,19 @@
 package com.tom.atm;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class FinanceActivity extends AppCompatActivity {
+
+    private MyDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,20 @@ public class FinanceActivity extends AppCompatActivity {
                 startActivity(new Intent(FinanceActivity.this, AddActivity.class));
             }
         });
+        ListView list = (ListView) findViewById(R.id.list);
+        dbHelper = new MyDBHelper(this, "expense.db", null, 1);
+        Cursor c = dbHelper.getReadableDatabase()
+                .query("exp", null, null, null, null, null, null);
+        String[] from = {"info", "amount"};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2,
+                c, from, to, 0);
+        list.setAdapter(adapter);
+
+
+//                .rawQuery("select * from users where userid=?", new String[]{"tom"});
+
     }
 
 }
